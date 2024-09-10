9b08dbda23ec90a1ea10dc61ccdbefebcda5280e
iramovchan <86199439+iramovchan@users.noreply.github.com>
2024-09-10 21:53:10 +0200

    second commit fixed

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
@@ -18,9 +17,9 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
-    // #ifdef __APPLE__
+    #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Fix compilation on macOS
-    // #endif
+    #endif
 
     // glfw window creation
     // --------------------
@@ -38,11 +37,11 @@ int main()
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+    // if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    // {
+    //     std::cout << "Failed to initialize GLAD" << std::endl;
+    //     return -1;
+    // }
 
     // Set initial viewport size
     glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
