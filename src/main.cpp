19bdcb9ef7a50d7a9983d7812c49a2b59ebc0017
iramovchan <irusya.movchan@gmail.com>
2024-09-16 14:02:44 +0200

    aaalmost done triangle on main, added separate files

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,44 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+GLfloat vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f,  0.5f, 0.0f
+};
+
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
+void checkCompileErrors(unsigned int shader, std::string type) {
+    int success;
+    char infoLog[512];
+    // glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(type != "PROGRAM"){
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        if(!success){
+            glGetShaderInfoLog(shader, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER_COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+    } else {
+        glGetProgramiv(shader, GL_LINK_STATUS, &success);
+        if(!success) {
+            glGetProgramInfoLog(shader, 512, NULL, infoLog);
+        }
+    }
+}
+
 int main()
 {
     // glfw: initialize and configure
@@ -38,6 +76,45 @@ int main()
     // Set initial viewport size
     glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 
+    unsigned int VBO; //vertex buffer object
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // sizeof size in bytes
+    // vertices = actual data we want to send
+    /* last parameter can have three forms:
+    GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
+    GL_STATIC_DRAW: the data is set only once and used many times.
+    GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
+    */
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    checkCompileErrors(vertexShader, "VERTEX");
+
+    // fragment shader - calculating the color output of pixels
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    checkCompileErrors(fragmentShader, "FRAGMENT");
+
+    //shader program - multiple shaders linked together
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    checkCompileErrors(shaderProgram, "PROGRAM");
+
+    // delete unlinked versions of shaders, not needed anymore
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
     // render loop
     while (!glfwWindowShouldClose(window))
     {
@@ -47,6 +124,8 @@ int main()
 		// rendering commands here
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);  // Set background color
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         glfwSwapBuffers(window);
